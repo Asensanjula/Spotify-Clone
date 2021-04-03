a370cf2f3c90ce81ebc0c058fe8788a163c1766c
Asen Sanjula <asensanjula@gmail.com>
2021-04-03 22:39:00 +0530

    add authentication with spotify web api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Login from "./Login";
+import {getTokenFromResponse} from "./Spotify";
 
 function App() {
+    const [token, setToken] = useState(null);
+    useEffect(() => {
+        const hash = getTokenFromResponse();
+        window.location.hash = "";
+        const _token = hash.access_token;
+
+        if (_token ){
+            setToken(_token)
+        }
+
+        console.log("I HAVE TOKEN >>> " , token);
+
+    },[])
+
   return (
     <div className="App">
-      <h1>Tesing Spotify</h1>
+        {
+            token ? <h1>I am Logged in</h1> : <Login/>
+        }
     </div>
   );
 }
